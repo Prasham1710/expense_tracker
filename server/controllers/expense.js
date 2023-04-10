3c921838c86b76a9c5e0d6c3f79c11db463fa1a5
Prasham1710 <jainprasham17@gmail.com>
2023-04-10 17:32:47 +0530

    expenses posted

--- server/controllers/expense.js
+++ server/controllers/expense.js
@@ -1,8 +1,8 @@
-const IncomeSchema = require("../models/incomeModel.js");
+const ExpenseSchema = require("../models/incomeModel.js");
 
-exports.addIncome = async (req, res) => {
+exports.addExpense = async (req, res) => {
     const { title, amount, category, description,date } = req.body;
-    const income = IncomeSchema ({
+    const income = ExpenseSchema ({
         title,
         amount,
         category,
@@ -18,26 +18,26 @@ exports.addIncome = async (req, res) => {
             return res.status(200).json({message: "Amount must  be a positive no."})
         }
         await income.save()
-        res.status(200).json({message : 'Income Added'})
+        res.status(200).json({message : 'Expense Added'})
     } catch (error) {
          res.status(200).json({message : 'Srever Error'})         
     }
     console.log(income)
 }
-exports.getIncomes = async (req,res) => {
+exports.getExpense = async (req,res) => {
     try {
-        const incomes = await IncomeSchema.find().sort({creeatedAt: -1})
+        const incomes = await ExpenseSchema.find().sort({creeatedAt: -1})
         res.status(200).json(incomes)
     } catch (error) {
         res.status(500).json({message : 'Srever Error'})
     }
 }
 
-exports.deleteIncome = async (req,res) => {
+exports.deleteExpense = async (req,res) => {
     const {id} = req.params;
-    IncomeSchema.findByIdAndDelete(id) 
+    ExpenseSchema.findByIdAndDelete(id) 
         .then((income) => {
-            res.status(200).json({message: 'Income Deleted'})     
+            res.status(200).json({message: 'Expense Deleted'})     
         })
         .catch((error) => {
             res.status(500).json({message: 'Server Error'})
--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -1,7 +1,11 @@
-const { addIncome } = require('../controllers/income');
-
+const { addIncome,getIncomes, deleteIncome } = require('../controllers/income');
+const { addExpense,getExpense,deleteExpense } = require('../controllers/expense');
 const router = require('express').Router();
 
-router.post('/add-income' , addIncome)
-
+router.post('/add-incomes' , addIncome)
+    .get('/get-incomes', getIncomes)
+    .delete('/delete-income/:id', deleteIncome)
+    .post('/add-expenses',addExpense)
+    .get('/get-expenses',getExpense)
+    .delete('/delete-expense/:id',deleteExpense)
 module.exports = router
