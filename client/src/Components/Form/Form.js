2e20bc6e6d43e07b7a87cdebc5fbeebdfe75a699
Prasham1710 <jainprasham17@gmail.com>
2023-04-16 01:53:40 +0530

    button

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
 import { useGLobalContext } from '../../context/globalContext';
+import { plus } from '../../utils/icons';
+import Button from '../Button/Button';
 function Form() {
     const {addIncome} = useGLobalContext()
     const [inputState, setInputState] = useState({
@@ -68,12 +70,60 @@ function Form() {
                 <textarea name="description" value={description} placeholder='Add A Reference' id="description" cols="30" rows="4" onChange={handleInput('description')}></textarea>
                 </div>
             <div className='submit-btn'>
-                <button>
-                    Add Income
-                </button>
+               <Button 
+                    name={'Add Income'}
+                    icon={plus}
+                    bPad={'.8rem 1.6rem'}
+                    bRad={'30px'}
+                    bg={'var(--color-accent'}
+                    color={'#fff'}
+                />
             </div></FormStyled>
   )
 }
 
-const FormStyled = styled.form``;
+const FormStyled = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    input, textarea, select{
+        font-family: inherit;
+        font-size: inherit;
+        outline: none;
+        border: none;
+        padding: .5rem 1rem;
+        border-radius: 5px;
+        border: 2px solid #fff;
+        background: transparent;
+        resize: none;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        color: rgba(34, 34, 96, 0.9);
+        &::placeholder{
+            color: rgba(34, 34, 96, 0.4);
+        }
+    }
+    .input-control{
+        input{
+            width: 100%;
+        }
+    }
+    .selects{
+        display: flex;
+        justify-content: flex-end;
+        select{
+            color: rgba(34, 34, 96, 0.4);
+            &:focus, &:active{
+                color: rgba(34, 34, 96, 1);
+            }
+        }
+    }
+    .submit-btn{
+        button{
+            box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+            &:hover{
+                background: var(--color-green) !important;
+            }
+        }
+    }
+`;
 export default Form
